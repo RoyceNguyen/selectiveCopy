5d6c65f4354ffd7f73d1327505af5432e04f6b7f
Royce Nguyen <Hoangtu.Nguyen11@stclairconnect.ca>
2019-03-16 23:31:57 -0400

    Initial commit

---
+++ selectiveCopy.py
@@ -0,0 +1,6 @@
+#a program that walks through a folder tree and searches for files with a certain file extension then copy from whatever location they are to a new folder
+
+import os
+import shutil
+
+
