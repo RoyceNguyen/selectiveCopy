12fd33fe65c9f4bfa4b2cfb14fa2048a69923ff5
Royce Nguyen <Hoangtu.Nguyen11@stclairconnect.ca>
2019-03-16 23:35:04 -0400

    go through folder and copy to destination

--- selectiveCopy.py
+++ selectiveCopy.py
@@ -2,5 +2,15 @@
 
 import os
 import shutil
+#prompt the user to enter the path, extension and destination
+path = input('Enter the absolute path of the directory you wish to copy from: ')
 
+ext = input("Enter the extension you would like to copy: ")
+
+destination = input("Enter destination folder's absolute filepath: ")
+#go through all the folders and files and copy the files to the destination folder
+for folders, subfolders, filenames in os.walk(path):
+    for filename in filenames:
+        if filename.endswith('{}'.format(ext)):
+            shutil.copy(os.path.join(folders, filename), destination)
 
